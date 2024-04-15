15f999539102e2304257aeceb071e071da7e0002
Maria Hernandez <mhernan5@mail.umw.edu>
2024-04-15 10:29:55 -0400

    Starting the main file

---
+++ culminationProject.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE HTML>
+<html>
+<header>
+	<meta charset="UTF-8">
+	<title>Culmination Project B: Implementation</title>
+	<link href=".css" rel="stylesheet" type="text/css">
+</header>
+<body>
+
+<?php
+//session variables
+?>
+
+</body>
+</html>
