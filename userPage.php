4102f39fcde9faad86812ad046dc83ff4e1f6272
Maria Hernandez <mhernan5@mail.umw.edu>
2024-04-22 13:50:22 -0400

    Adding the userPage and guestPage

---
+++ userPage.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+if(isset($_SESSION['Username'])) {
+    echo "You are logged in " . $_SESSION['Username'];
+}
+?>
+
+<!DOCTYPE HTML>
+<html>
+    <header>
+        <meta charset="UTF-8">
+        <title>Culmination Project B: Implementation</title>
+        <link href="StyleSheet.css" rel="stylesheet" type="text/css">
+    </header>
+
+    <body>
+        <p>What's Poppin?</p>
+
+    <?php
+    //session variables
+    ?>
+
+    </body>
+</html>
+
